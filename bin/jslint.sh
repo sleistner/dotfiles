3faa013e0971bcffb77886aea03db37631cdf567
sleistner <sleistner@gmail.com>
2010-08-23 21:04:57 +0200

    unused scripts removed

--- bin/jslint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/System/Library/Frameworks/JavaScriptCore.framework/Resources/jsc ~/.vim/plugin/jslint/parser.js -- "/Users/sleistner/.vim/plugin/jslint/fulljslint.js" "$1" "$(cat $1)"
-
